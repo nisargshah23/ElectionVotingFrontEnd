9f02f26c0984fb9c9e5f8150ed0a550c89f54d50
ravi <patodiyaravi680@gmail.com>
2024-12-20 12:27:23 +0530

    login page and voting page complete

--- FrontEnd/ElectionVoting/src/App.jsx
+++ FrontEnd/ElectionVoting/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import LoginPage from './component/LoginPage'
 import { VerificationCountDisplay } from './component/verificationCount'
+import VoteParty from './component/VoteParty'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <VerificationCountDisplay/>
+      {/* <VerificationCountDisplay/> */}
+      {/* <LoginPage /> */}
+      <VoteParty />
     </>
   )
 }
---
+++ FrontEnd/ElectionVoting/src/component/LoginPage.jsx
@@ -0,0 +1,53 @@
+
+import React, { useState } from 'react';
+import '../assets/styles/Login.css'
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!username || !password) {
+      setError('Both fields are required.');
+      return;
+    }
+    setError('');
+    console.log(username , password)
+    setUsername('')
+    setPassword('')
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter your username"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+          />
+        </div>
+        {error && <p className="error-message">{error}</p>}
+        <button type="submit" className="login-button">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
